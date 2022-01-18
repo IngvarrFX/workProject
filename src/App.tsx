606d8bbce258b962342cf7613f846b28c6fc3207
IngvarrFX <64922169+IngvarrFX@users.noreply.github.com>
2022-01-18 16:12:35 +0300

    project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import "./App.css";
+import {useSelector} from "react-redux";
+import {AppStateType} from "./store/store";
+import {privateRoutes, publicRoutes} from "./components/routes/routes";
+import {BrowserRouter, Navigate, Route, Routes, useNavigate} from "react-router-dom";
+import {Login} from "./components/login/Login";
+import {Main} from "./components/main/Main";
+
 
 function App() {
-  return (
-    <div className="App">
-        <h1>Project</h1>
-        <h2>test</h2>
-    </div>
-  );
+    const isAuth = useSelector<AppStateType, boolean>(state => state.app.isAuth)
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (!isAuth) {
+            navigate("/login")
+        } else {
+            navigate("/main")
+        }
+    }, [])
+
+    return (
+        <div className={"App"}>
+            {!isAuth
+                ?
+                /*<Routes>
+                    <Route path={"/login"} element={<Login/>}/>
+                </Routes>*/
+                <Login/>
+                :
+               /* <Routes>
+                    <Route path={"/main"} element={<Main/>}/>
+                </Routes>*/
+                <Main/>
+            }
+        </div>
+    );
 }
 
 export default App;
